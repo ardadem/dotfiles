5a7ebb34abc561872b5ab77fafcd756d84994753
Arda <ddmirarda@gmail.com>
2019-07-22 21:29:25 +0300

    being honest

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-ln -s /home/$USER/.cfgs/i3/config ~/.config/i3/
-ln -s /home/$USER/.cfgs/compton/compton.conf ~/.config/i3/
+# i'm too lazy for type 5 or 6 lines :d
+#ln -s /home/$USER/.cfgs/i3/config ~/.config/i3/
+#ln -s /home/$USER/.cfgs/compton/compton.conf ~/.config/i3/
